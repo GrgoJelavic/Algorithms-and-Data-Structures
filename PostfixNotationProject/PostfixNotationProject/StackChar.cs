c83d42116c8f9d5de79d9590e7cc002f196a6078
Grgo <9r9o99@gmail.com>
2020-09-25 00:24:10 +0200

    Reverse Polish notation project - implementation using stack array

---
+++ PostfixNotationProject/PostfixNotationProject/StackChar.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostfixNotationProject
+{
+    class StackChar
+    {
+        private char[] stackArray;
+        private int top;
+
+        public StackChar()
+        {
+            stackArray = new Char[10];
+            top = -1;
+        }
+
+        public StackChar(int MaxSize)
+        {
+            stackArray = new Char[MaxSize];
+            top = -1;
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool IsFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+
+        public void Push(char x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack Overflow\n");
+                return;
+            }
+            top = top + 1;
+            stackArray[top] = x;
+        }
+
+        public char Pop()
+        {
+            char x;
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow\n");
+                throw new System.InvalidOperationException();
+            }
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        }
+
+        public char Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow\n");
+                throw new System.InvalidOperationException();
+            }
+            return stackArray[top];
+        }
+    }
+}
---
+++ PostfixNotationProject/PostfixNotationProject/StackInt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostfixNotationProject
+{
+    class StackInt
+    {
+        private int[] stackArray;
+        private int top;
+
+        public StackInt()
+        {
+            stackArray = new int[10];
+            top = -1;
+        }
+
+        public StackInt(int MaxSize)
+        {
+            stackArray = new int[MaxSize];
+            top = -1;
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool IsFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+
+        public void Push(int x)
+        {
+            if (IsFull())
+                Console.WriteLine("Stack Overflow\n");
+            else
+            {
+                top = top + 1;
+                stackArray[top] = x;
+            }
+        }
+
+        public int Pop()
+        {
+            int x;
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow\n");
+                throw new System.InvalidOperationException();
+            }
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        }
+    }
+}
