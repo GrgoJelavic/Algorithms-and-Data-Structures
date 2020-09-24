c8c38d1e7f6146115daf0063a98df14894fe3c4c
Grgo <9r9o99@gmail.com>
2020-09-24 23:41:19 +0200

    Stack project - implementation using linked list and array

---
+++ StackProject/StackProject/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+            StackLinkedList stackL = new StackLinkedList();
+
+            while (true)
+            {
+                Console.WriteLine("1. Push element");
+                Console.WriteLine("2. Pop element");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Display elements");
+                Console.WriteLine("5. Display size");
+                Console.WriteLine("6. Exit");
+                do
+                    Console.WriteLine("\nEnter your choice: ");
+                while (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out choice));
+                if (choice == 6)
+                    break;
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        do
+                            Console.Write("Push element: ");
+                        while (!int.TryParse(Console.ReadLine(), out x));
+                        stackL.Push(x);
+                        break;
+                    case 2:
+                        do
+                            Console.Write("Pop element: ");
+                        while (!int.TryParse(Console.ReadLine(), out x));
+                        stackL.Pop();
+                        break;
+                    case 3:
+                        Console.WriteLine("Peek: " + stackL.Peek());
+                        break;
+                    case 4:
+                        stackL.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Stack size: " + stackL.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice! Try again!");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    
+}
---
+++ StackProject/StackProject/StackLinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject
+{
+    class StackLinkedList
+    {
+        private Node top;
+
+        public StackLinkedList()
+        {
+            top = null;
+        }
+
+        public int Size()
+        {
+            int size = 0;
+            Node p = top;
+            while (p != null)
+            {
+                p = p.link;
+                size++;
+            }
+            return size;
+        }
+
+        public void Push(int x)
+        {
+            Node temp = new Node(x);
+            temp.link = top;
+            top = temp;
+        }
+
+        public int Pop()
+        {
+            int x;
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Stack Underflow");
+            x = top.info;
+            top = top.link;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Stack Underflow");
+            return top.info;
+        }
+
+        public bool IsEmpty()
+        {
+            return (top == null);
+        }
+
+        public void Display()
+        {
+            Node p = top;
+            if (IsEmpty())
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                Console.WriteLine("Stack display: ");
+                while (p != null)
+                {
+                    Console.WriteLine(p.info + " ");
+                    p = p.link;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
